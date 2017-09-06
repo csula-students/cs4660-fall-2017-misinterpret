9f6d3d702a8784ea359b312bdb1df1fdae86f6bc
misinterpret <zodiackillerg@gmail.com>
2017-09-06 14:26:29 -0700

    Did the week 2 exercises

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,4 +1,5 @@
 """Files tests simple file read related operations"""
+from __future__ import division
 
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
@@ -9,30 +10,35 @@ class SimpleFile(object):
         dimension array (numbers)
         """
 
+        f = open(file_path)
+
+        for x in f:
+            self.numbers.append(list(map(int,x.split(" "))))
+
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        return sum(self.numbers[line_number])/len(self.numbers[line_number])
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return max(self.numbers[line_number])
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        return min(self.numbers[line_number])
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        return sum(self.numbers[line_number])
--- cs4660/tutorial/lists.py
+++ cs4660/tutorial/lists.py
@@ -1,21 +1,16 @@
 """Lists defines simple list related operations"""
 
 def get_first_item(li):
-    """Return the first item from the list"""
-    pass
+    return li[0]
 
 def get_last_item(li):
-    """Return the last item from the list"""
-    pass
+    return li[-1]
 
 def get_second_and_third_items(li):
-    """Return second and third item from the list"""
-    pass
+    return [li[1],li[2]]
 
 def get_sum(li):
-    """Return the sum of the list items"""
-    pass
+    return sum(li)
 
 def get_avg(li):
-    """Returns the average of the list items"""
-    pass
+    return sum(li)/len(li)
